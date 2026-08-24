1c4c7f4b37ad3dd293770a31003cfcdd41ba2e7a
joyni <joyniranjan21@gmail.com>
2026-08-24 14:41:01 -0400

    Updated rectangle corner colors

--- drawstuff.js
+++ drawstuff.js
@@ -158,9 +158,9 @@ function main() {
     var imagedata = context.createImageData(w,h);
  
     // Define a rectangle in 2D with colors and coords at corners
-    var ulc = new Color(255,0,0,255); // upper left corner color: red
-    var urc = new Color(0,255,0,255); // upper right corner color: green
-    var llc = new Color(0,0,255,255); // lower left corner color: blue
+    var ulc = new Color(255,0,255,255); // upper left corner color: yellow
+    var urc = new Color(0,255,255,255); // upper right corner color: cyan
+    var llc = new Color(255,255,0,255); // lower left corner color: magenta
     var lrc = new Color(0,0,0,255); // lower right corner color: black
     var ulx = 50, uly = 50; // upper left corner position
     var urx = 200, ury = 50; // upper right corner position
